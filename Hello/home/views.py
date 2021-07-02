7bf9826526a989bb803acaff1b252994802af245
Amruta <amruta.pendse@teqfocus.com>
2021-07-02 15:41:13 +0530

    Added menu option

--- Hello/home/views.py
+++ Hello/home/views.py
@@ -6,3 +6,9 @@ def index(request):
 
 def about(request):
     return HttpResponse("This is about")
+
+def services(request):
+    return HttpResponse("This is services")
+
+def contact(request):
+    return HttpResponse("This is contact")
